333a611a5c7b36002c70a1ca6657d244c41b269c
unknown <aryodimas07@gmail.com>
2018-05-26 12:38:58 +0700

    initial files

---
+++ src/main/java/com/filkom/pulmart/PulmartApplication.java
@@ -0,0 +1,12 @@
+package com.filkom.pulmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulmartApplication.class, args);
+	}
+}
